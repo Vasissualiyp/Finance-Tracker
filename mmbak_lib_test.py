3a8203828035b2d7c0065f49a8b2643b881e5ca8
Vasilii Pusovoit <vasily.pustovoit@mail.utoronto.ca>
2023-12-30 23:32:02 -0500

    Now I can extract income and outcome from the mmbak file

--- mmbak_lib_test.py
+++ mmbak_lib_test.py
@@ -2,6 +2,7 @@ from mmbak_analysis_lib.db_driver import DbDriver
 from mmbak_analysis_lib.data_analysis import plot_data
 from mmbak_analysis_lib.categories_analysis import get_active_categories
 from mmbak_analysis_lib.currencies_analysis import get_currencies
+from datetime import datetime, timezone
 
 # Path to your mmbak file
 mmbak_file_path = 'MMAuto[GF231230](2023-12-30-115403).mmbak'
@@ -10,9 +11,23 @@ mmbak_file_path = 'MMAuto[GF231230](2023-12-30-115403).mmbak'
 db_driver = DbDriver(mmbak_file_path)
 
 # Example: Fetch and plot income/outcome data
-query = "SELECT ZDATE, ZMONEY FROM INOUTCOME WHERE IS_DEL = 0;"
-income_outcome_df = db_driver.query_to_dataframe(query)
-plot_data(income_outcome_df, 'Income/Outcome Over Time', 'ZDATE', 'ZMONEY', log_scale=True)
+#query = "SELECT ZDATE, ZMONEY FROM INOUTCOME WHERE IS_DEL = 0;"
+income_query =   "SELECT ZDATE, ASSET_NIC, ZMONEY FROM INOUTCOME WHERE DO_TYPE = 0;"
+outcome_query =  "SELECT ZDATE, ASSET_NIC, ZMONEY FROM INOUTCOME WHERE DO_TYPE = 1;"
+transfer_query = "SELECT ZDATE, ASSET_NIC, WDATE, toAssetUid, ZMONEY FROM INOUTCOME WHERE DO_TYPE = 3;"
+income_df = db_driver.query_to_dataframe(income_query)
+outcome_df = db_driver.query_to_dataframe(outcome_query)
+transfer_df = db_driver.query_to_dataframe(transfer_query)
+# Convert each timestamp to a readable date format
+income_df['formatted_date']   =   income_df['ZDATE'].apply(lambda x: datetime.fromtimestamp(int(x) / 1000, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S UTC'))
+outcome_df['formatted_date']  =  outcome_df['ZDATE'].apply(lambda x: datetime.fromtimestamp(int(x) / 1000, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S UTC'))
+transfer_df['formatted_date'] = transfer_df['ZDATE'].apply(lambda x: datetime.fromtimestamp(int(x) / 1000, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S UTC'))
+
+print(income_df)
+print(outcome_df)
+print(transfer_df)
+"""
+#plot_data(income_outcome_df, 'Income/Outcome Over Time', 'ZDATE', 'ZMONEY', log_scale=True)
 
 # Fetch active categories
 categories_df = get_active_categories(mmbak_file_path)
@@ -26,6 +41,6 @@ print(currencies_df)
 
 # Close the database connection
 db_driver.close()
-
+"""
 
 
